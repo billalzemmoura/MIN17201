550a2f084dccd9c56541da9df37281356d1fe9af
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-02-28 19:08:49 +0100

    oraganisation des fichiers par dossier

---
+++ Exercice3.1/SRP/src/main/java/td1/SRP/Afficher.java
@@ -0,0 +1,18 @@
+package td1.SRP;
+
+public class Afficher {
+
+	public void   AfficherCoordone(String message) {
+	System.out.println(message);
+    
+	}	
+	public void   AfficherSalaire(double salaire) {
+	System.out.println(salaire);
+    
+	}	
+
+	
+	
+
+
+}
