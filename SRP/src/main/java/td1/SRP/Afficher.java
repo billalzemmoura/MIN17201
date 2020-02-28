54f7c005b95161ecd8c100fd569428f35addbfca
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-02-28 17:41:11 +0100

    création de la classe Afficher qui a pour responsabilité d'afficher les attributs de la classe Employe

---
+++ SRP/src/main/java/td1/SRP/Afficher.java
@@ -0,0 +1,16 @@
+package td1.SRP;
+
+public class Afficher {
+
+	public void   AfficherCoordone(String message) {
+	System.out.println(message);
+    
+	}	
+	public void   AfficherSalaire(double salaire) {
+	System.out.println(salaire);
+    
+	}	
+
+
+
+}
